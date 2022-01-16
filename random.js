485831a54b988184ff5736040f4aef4d0bb485fb
Carson-Fletcher <96039122+Carson-Fletcher@users.noreply.github.com>
2022-01-16 16:26:02 -0700

    updated to the latest version

--- random.js
+++ random.js
@@ -1,86 +1,101 @@
-/**
- * @version 1.1
- */
-export const random = {
-    /**
-     * This method is used to simplifie the generation of random integers
-     * between a given min, and max(inclusive eg. number generated with
-     * random.randInt(1-3) would generate 1, 2 & 3 not just 1 & 2). 
-     * 
-     * @param {number} min Minimum number for generation
-     * @param {number} max Maximum number for generation 
-     * @returns {number} Returns the random number
-     * @throws {TypeError} If the args are not valid ie not a number it will throw them.
-     */
+/* random.js version 1.2 https://github.com/Carson-Fletcher/Random.js/README */
+const random = {
     randInt(min, max) {
-        if (typeof min != 'undefined' && typeof min != 'number') {
-            throw new TypeError(`Expected integer. Received ${typeof min}: ${min}`);
-        } else if (typeof max != 'undefined' && typeof max != 'number') {
-            throw new TypeError(`Expected integer. Received ${typeof max}: ${max}`);
-        }
-
         if (arguments.length == 1) {
             max = min;
             min = 0;
-        } else if (arguments.length == 0) {
+        }
+
+        if (arguments.length == 0) {
             max = 1;
             min = 0;
         }
 
-        return Math.floor(Math.random() * (max - min + 1)) + min;        
-    },
-
-    /**
-     * This method will return a random boolean based on a minimum passing value. This
-     * minPass value allows you to call this and have the output favour either true or
-     * false. (eg: random.randBool(0.125) has a higher change of returning true then value)
-     * 
-     * @param {number} minPass Minimum value to return true. Higher value means less likely to return true.
-     * @returns {boolean} Returns either true or false.
-     * @throws {TypeError} If the value of the argument this method is called with is not either a number or undefined it will throw it.
-     */
-    randBool(minPass) {
-        if (typeof minPass != 'number' && typeof minPass != 'undefined') {
-            throw new TypeError(`Expected integer. Received ${typeof minPass}: ${minPass}`);
+        if (!Number.isInteger(min)) {
+            throw TypeError(`Expected integer. Received ${typeof min}: ${min}`);
         }
-    
-        minPass = typeof minPass == 'undefined' ? 0.5 : parseFloat(minPass.toFixed(4));
-        
-        return parseFloat(Math.random().toFixed(4)) > minPass ? true : false;
-    },
 
-    /**
-     * This method will random pick an index from an array or string called as an
-     * argument.
-     * 
-     * @param {Array} arr You must pass this method either a string or array.
-     * @returns {number} Returns a random number between 0 and the max index
-     * of the passed string/array.
-     * @throws {TypeError} If the argument is not an array then it throws it.
-     */
-    randIndexOfArray: function(arr) {
-        if ((!Array.isArray(arr) && typeof arr != 'string') || arguments.length != 1) {
+        if (!Number.isInteger(max)) {
+            throw TypeError(`Expected integer. Received ${typeof max}: ${max}`);
+        }
+
+        if (max < min) {
+            [max, min] = [min, max];
+        }
+        
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+    randFloat(min, max, fixed) {
+        if (arguments.length == 2) fixed = 2;
+        if (arguments.length == 1) {
+            max = min;
+            min = 0;
+            fixed = 2;
+        }
+
+        if (arguments.length == 0) {
+            max = 1;
+            min = 0;
+            fixed = 2;
+        }
+
+        if (isNaN(min)) {
+            throw TypeError(`Expected integer or float. Received ${typeof min}: ${min}`);
+        }
+
+        if (isNaN(max)) {
+            throw TypeError(`Expected integer or float. Received ${typeof max}: ${max}`);
+        }
+
+        if (!Number.isInteger(fixed) || fixed < 0) {
+            throw TypeError(`Expected positive integer. Received ${typeof fixed}: ${fixed}`);
+        }
+
+        if (max < min) {
+            [max, min] = [min, max];
+        }
+
+        return parseFloat((Math.random() * (max - min) + min).toFixed(fixed));
+    },
+    randBool() {
+        return Math.random() + 0.5 >> 0 == 1;
+    },
+    randLet() {
+        const letters = "abcdefghijklmnopqrstuvwxyz";
+        return letters[Math.floor(Math.random() * ((letters.length - 1) + 1))]
+    },
+    randChoice(arr) {
+        if ((!arguments.length && arr === undefined) || (!Array.isArray(arr) && typeof arr !== 'string')) {
             throw new TypeError(`Expected array or string. Received ${typeof arr}: ${arr}`);
         }
 
-        return Math.floor(Math.random() * ((arr.length - 1) + 1));
+        return arr[Math.floor(Math.random() * ((arr.length - 1) + 1))];
     },
-
-    /**
-     * This method will return a random letter from a-z with
-     * capitilization if specified.
-     * 
-     * @param {boolean} toCaps Determines if the method should capitilize the letter.
-     * @returns {string} Returns random letter from a-z either capitilized or not.
-     * @throws {TypeError} If the argument is not a boolean it throws it.
-     */
-    randLetter(toCaps) {
-        if (typeof toCaps != 'boolean' && typeof toCaps != 'undefined') {
-            throw new TypeError(`Expected boolean. Received ${typeof toCaps}: ${toCaps}`)
+    shuffleArr(arr) {
+        if ((!arguments.length && !arr.length) || (!Array.isArray(arr))) {
+            throw new TypeError(`Expected array. Received ${typeof arr}: ${arr}`);
         }
 
-        const letters = "abcdefghijklmnopqrstuvwxyz";        
+        for (let i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
 
-        return (toCaps) ? letters[Math.floor(Math.random() * ((letters.length - 1) + 1))].toUpperCase() : letters[Math.floor(Math.random() * ((letters.length - 1) + 1))];
+        return arr;
     },
+    shuffleStr(str) {
+        if ((!arguments.length && !arr.length) || (typeof str !== 'string')) {
+            throw new TypeError(`Expected string. Received ${typeof str}: ${str}`);
+        }
+
+        str = str.split("");
+        let n = str.length;
+
+        for(let i = n - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [str[i], str[j]] = [str[j], str[i]]
+        }
+        
+        return str.join("");
+    }
 };
